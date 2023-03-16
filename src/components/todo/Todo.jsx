4e869b18756c4fc6350b46c46c1bc80a682bbb7f
tpiv <ds@tpiv.ru>
2023-03-16 14:00:07 +0300

    1

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,56 +1,27 @@
 import './todo.css'
-import { useRef, useState } from 'react';
+import TodoList from '../todoList/TodoList';
+import { useState } from 'react';
 
+let numbers = [{id:1, text: "Выпить", checked: false}, {id:2, text: "Закусить", checked: false}, {id:3, text: "Покурить", checked: false}];
 export default function InputDate() {
     //логика, функция
-    const color = useRef();
-    let numbers = ["Выпить", "Закусить", "Поиграть в Postal 2", "Набить морду страусу", "покурить"];
     const [tasks, setTasks] = useState(numbers);
     function add () {
-        setTasks([...tasks, ""]);
+        let id = numbers[numbers.length-1].id + 1;
+        setTasks([...tasks, {id: id, text: "", checked: false}]);
     }
-    function textTask (event) {
-        // console.log(event);
-        let mass = Array.from(tasks);
-        mass[event.target.id] = event.target.value;
-        setTasks(mass);
-    }
-    function delTask (event) {
-        console.log(event.target.id);
-        let id = parseInt(event.target.id);
-        let mass = [];
-        for(let i=0; i < tasks.length; i++) {
-            if(i !== id) {
-                mass.push(tasks[i]);
-            }
-        }
-        setTasks(mass);
-    }
-    function checked(event) {
-        // console.log(event.target);
-        if(event.target.checked) {
-            //сделать цвет зеленым
-            // console.log(color.current);
-            color.current.setAttribute("style", "color: green;");
-        }
-        else {
-            //сделать цвет обычным
-            // console.log(color.current);
-            color.current.setAttribute("style", "color: black;");
-        }
-    }
-
     return (
         <div className='wrap'>
             <button className="but" onClick={add}>Добавить заметку</button>
             {
-                tasks.map((num, index)=>(
-                <div className='count' key={index}>
-                <div><input type="checkbox" name="ok" onChange={checked} /></div>
-                <div><input ref={color} type="text" className='wi' id={index} value={num} onChange={textTask} /></div>
-                <div><button onClick={delTask} id={index}>Удалить</button></div>
-                </div>
-                ))
+                tasks.map(t=>
+                <TodoList
+                    key={t.id}
+                    elem={t}
+                    tasks={tasks}
+                    setTasks={setTasks}
+                />
+                )
             }
         </div>
     )
---
+++ src/components/todoList/TodoList.jsx
@@ -0,0 +1,36 @@
+import './todoList.css'
+
+export default function InputDate(props) {
+    //логика, функция
+    const {elem, tasks, setTasks} = {...props};
+
+    function textTask (event) {
+        setTasks(tasks.map((obj, i) => {
+            if(elem.id===i) {
+                obj.text=event.target.value;
+            }
+            return obj;
+        }));
+    }
+    function delTask (event) {
+        console.log(event.target.id);
+        let id = parseInt(event.target.id);
+        let mass = [];
+        for(let i=0; i < tasks.length; i++) {
+            if(i !== id) {
+                mass.push(tasks[i]);
+            }
+        }
+        setTasks(mass);
+    }
+    function checked(event) {
+        console.log(event.target.id);
+    }
+    return (
+        <div className='count'>
+        <div><input type="checkbox" name="ok" onChange={checked} /></div>
+        <div><input type="text" className='wi' value={elem.text} onChange={textTask} /></div>
+        <div><button onClick={delTask}>Удалить</button></div>
+        </div>
+    )
+}
